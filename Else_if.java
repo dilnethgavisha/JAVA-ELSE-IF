514137ab9c88a12e091592416f6e6272d45458cd
dilnethgavisha <dilnethgavisha2@gmail.com>
2025-01-31 17:01:10 +0530

    test 1

--- Else_if.java
+++ Else_if.java
@@ -1,8 +1,12 @@
 public class Else_if{
     public static void main(String [] args){
-        int = 1;
-        if(x < 0){
-            System.out.println("x is negative");
+        int time = 22;
+        if(time < 10){
+            System.out.println("Good morning");
+        }else if(time > 10){
+            System.out.println("Good day");
+        }else(time == 22){
+            System.out.println("Good evening");
         }
     }
 }
