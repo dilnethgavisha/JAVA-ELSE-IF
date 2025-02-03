073292db02e37b9e8bf1aa5cc3fa4ed5afbdcf26
dilnethgavisha <dilnethgavisha2@gmail.com>
2025-02-03 17:32:09 +0530

    test

--- Else_if.java
+++ Else_if.java
@@ -3,10 +3,11 @@ public class Else_if{
         int time = 22;
         if(time < 10){
             System.out.println("Good morning");
-        }else if(time > 10){
+        }else if(time > 10 && time < 22){
             System.out.println("Good day");
-        }else(time == 22){
+        }else{
             System.out.println("Good evening");
+            System.out.println("Have a great night");
         }
     }
 }
